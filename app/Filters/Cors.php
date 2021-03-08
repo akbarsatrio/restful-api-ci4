f696737ec6dbd0fab797930956786d5a8d706296
akbarsatrio <akbarsatrio@outlook.co.id>
2021-03-08 14:30:58 +0700

    cors method

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -25,7 +25,11 @@ class Cors implements FilterInterface
 	 */
 	public function before(RequestInterface $request, $arguments = null)
 	{
-		//
+		header('Access-Control-Allow-Origin: *');
+			header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+			header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+			$method = $_SERVER['REQUEST_METHOD'];
+			die() ? $method == "OPTIONS" : null;
 	}
 
 	/**
